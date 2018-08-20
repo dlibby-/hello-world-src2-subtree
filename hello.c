f493c54e26530201b689e707487baac0f079b36a
Daniel Libby <dlibby@microsoft.com>
2018-08-20 13:08:37 -0700

    Initial commit

---
+++ hello.c
@@ -0,0 +1,20 @@
+// Copyright 2012 The Goma Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+  printf("Hello world\n");
+  time_t start;
+  time(&start); 
+  while(1) {
+    time_t now;
+    time(&now);
+    if (now - start > 120) {
+      break;
+    }
+  }
+  return 0;
+}
