f4670a44b482e703b93e355490c0e2d040df4092
Daniel Libby <dlibby@microsoft.com>
2018-08-21 14:31:44 -0700

    Remove extranewlines
    Signed-off-by: Daniel Libby <dlibby@microsoft.com>

--- hello.c
+++ hello.c
@@ -1,18 +1,14 @@
 // Copyright 2012 The Goma Authors. All rights reserved.
-
 // Use of this source code is governed by a BSD-style license that can be
-
 // found in the LICENSE file.
 
 #include <stdio.h>
-
 #include <time.h>
 
 int main() {
   printf("Hello world\n");
   time_t start;
   time(&start); 
-
   while(1) {
     time_t now, later;
     time(&now);
